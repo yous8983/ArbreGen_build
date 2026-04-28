ebe14766ce2e53577fc199d5412f6046fe4fbb06
yous8983 <traoreosman@yahoo.fr>
2026-04-28 22:29:45 +0200

    fix: declare CSS modules for TypeScript

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="react-scripts" />
 declare namespace JSX { interface IntrinsicElements { [e: string]: any; } }
+
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
