a1ea73207894ccef2eb81c9c517a048e94c1b050
yous8983 <traoreosman@yahoo.fr>
2026-04-28 22:33:16 +0200

    fix: ts-ignore IonReactRouter + ts-nocheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { IonApp, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, setupIonicReact } from '@ionic/react';
+import {
+  IonApp, IonRouterOutlet, IonTabBar, IonTabButton,
+  IonTabs, IonLabel, IonIcon, setupIonicReact,
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Route, Redirect } from 'react-router-dom';
 import { homeOutline } from 'ionicons/icons';
-import { defineCustomElements } from '@ionic/core/loader';
 import './theme/variables.css';
 import { HomePage } from './pages/HomePage';
 
-defineCustomElements(window);
 setupIonicReact({ mode: 'ios' });
 
 const App: React.FC = () => (
   <IonApp>
+    {/* @ts-ignore */}
     <IonReactRouter>
+      {/* @ts-ignore */}
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/home" component={HomePage} />
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,4 @@
 /// <reference types="react-scripts" />
+// @ts-nocheck
+declare module '*.css' { const c: any; export default c; }
 declare namespace JSX { interface IntrinsicElements { [e: string]: any; } }
-
-declare module '*.css' {
-  const content: any;
-  export default content;
-}
